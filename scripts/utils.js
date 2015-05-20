d838cf92c21d71413ff9c75c21cdecd9ac70e059
Mimerme <androssdragonq.9@gmail.com>
2015-05-20 19:43:47 -0400

    Added support for adding games Added messageBox utility

---
+++ scripts/utils.js
@@ -0,0 +1,95 @@
+//Read and write configs & create new windows
+var fs = require('fs');
+var exec = require('child_process').execFile;
+
+var date = new Date();
+
+var windows = {};
+
+var writeConfig = function writeToConfig(path, data){
+  var dat = JSON.stringify(data);
+fs.writeFile(path, dat, function(err) {
+    if(err) {
+        alert("there was an error creating the config file, check the console for output");
+        console.log(err);
+    }
+});
+};
+
+var writeLog = function writeToLog(data){
+  fs.appendFile("latest.log", "[" + date.getMilliseconds() + "] " +  data, function(err) {
+      if(err) {
+          alert("there was an error creating the log file, check the console for output");
+          console.log(err);
+      }
+  });
+};
+
+var readConfig = function readFromConfig(path){
+  var data = fs.readFileSync(path),
+      myObj;
+
+  try {
+    myObj = JSON.parse(data);
+  }
+  catch (err) {
+    console.log('There has been an error reading your JSON.')
+    console.log(err);
+  }
+  return myObj;
+};
+
+//returns new window object
+var newWindow = function newWindow(source, width, height, title, func){
+  if(windows[source] == null){
+  var win = global.window.nwDispatcher.requireNwGui().Window.open("../../skin/default/window_frame.html?id=" + source, {
+    toolbar: true,
+    frame: false,
+    position: 'center',
+    width: width,
+    height: height,
+    min_width: width,
+    min_height: height,
+    max_width: width,
+    max_height: height
+  });
+  win.minimize();
+  win.restore();
+  win.on('close', function() {
+    windows[source] = null;
+    win.close(true);
+  });
+  if(func != null){
+    win.on('document-end', func);
+  }
+  windows[source] = win;
+}
+else{
+  windows[source].show();
+  windows[source].focus();
+}
+return windows[source];
+};
+
+var runApp =function(path){
+   console.log("Starting " + path);
+   exec(path, function(err, data) {
+        console.log(err)
+        console.log(data.toString());
+    });
+}
+
+var popupMessage = function(tvar){
+  newWindow("pages/Misc/message_box_okay.html" ,600, 200, "New PopupMessasge",
+  function(frame){
+     frame.contentDocument.body.innerHTML =
+     frame.contentDocument.body.innerHTML.replace('[Text goes here]', tvar);
+    });
+}
+
+global.popupMessage = popupMessage;
+global.writeConfig = writeConfig;
+global.writeLog = writeLog;
+global.readConfig = readConfig;
+global.newWindow = newWindow;
+global.runApp = runApp;
--- skin/default/pages/Games/game_screen.js
+++ skin/default/pages/Games/game_screen.js
@@ -1,23 +1,35 @@
 var os = require('os');
 
-console.log(__dirname);
+//should match the contents of games.cfg at all times
+//any modifications should also fire saveGames();
+var gameList = {
+};
 
-var gameList = {};
-
+//addGame() should be fired every time a game is added
+//to the hastable
 function addGame(name, path){
   gameList[name] = path;
+  saveGames();
+}
+
+function removeGame(name, path){
+  gameList[name] = null;
+  saveGames();
 }
 
 function loadGames(){
   console.log("Loaded the following hastable");
   console.log("------------------------------");
+  gameList = global.readConfig('configs/default/games.cfg');
   console.log(gameList);
 }
 
+//should be fired whenever gameList is modified aka whenever
+//add/removeGame() is called
 function saveGames(){
-  configUtil.writeConfig("games.cfg", gameList);
+  global.writeConfig('configs/default/games.cfg', gameList);
 }
 
 function newWindow(src, w, h, id){
-  configUtil.newWindow(src, w, h, id);
+  global.newWindow(src, w, h, id);
 }
